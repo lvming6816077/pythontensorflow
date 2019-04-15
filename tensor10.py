03a72311375e435e8d1b21ecd2f4f75c8342e3eb
lvminga@yutong.com <lvminga@yutong.com>
2019-04-15 17:26:15 +0800

    edadasdf

--- tensor10.py
+++ tensor10.py
@@ -7,44 +7,45 @@ import tensorflow as tf
 # from PIL import Image
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt # plt 用于显示图片
+from PIL import Image, ImageFilter
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/",one_hot=True)
 
-# from mnist import MNIST
+from mnist import MNIST
 
-# mndata = MNIST(path='MNIST_data/',gz=True)
+mndata = MNIST(path='MNIST_data/',gz=True)
 
-# x = tf.placeholder("float", [None, 784])  # x定义为占位符，待计算图运行的时候才去读取数据图片
-# W = tf.Variable(tf.zeros([784, 10]))      # 权重w初始化为0
-# b = tf.Variable(tf.zeros([10]))           # b也初始化为0
-# y = tf.nn.softmax(tf.matmul(x, W) + b)    # 创建线性模型
-# y_ = tf.placeholder("float", [None, 10])  # 图片的实际标签，为0~9的数字
+x = tf.placeholder(tf.float32, [None, 784])  # x定义为占位符，待计算图运行的时候才去读取数据图片
+W = tf.Variable(tf.zeros([784, 10]))      # 权重w初始化为0
+b = tf.Variable(tf.zeros([10]))           # b也初始化为0
+y = tf.nn.softmax(tf.matmul(x, W) + b)    # 创建线性模型
+y_ = tf.placeholder(tf.float32, [None, 10])  # 图片的实际标签，为0~9的数字
 
 
 
-# # 使用Tensorflow自带的交叉熵函数
-# cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y)
-# train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-# sess = tf.Session()
-# # 变量初始化
-# init = tf.global_variables_initializer()
-# sess.run(init)
-# saver = tf.train.Saver()
-# for i in range(1000):
-#     batch_xs, batch_ys = mnist.train.next_batch(500)
-#     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+# 使用Tensorflow自带的交叉熵函数
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y)
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+sess = tf.Session()
+# 变量初始化
+init = tf.global_variables_initializer()
+sess.run(init)
+saver = tf.train.Saver()
+for i in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(500)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
 # save_path = saver.save(sess, "./mnistsoftmax/my_model_final.ckpt")
 
-# import cv2 
-# images, labels = mndata.load_testing()
-# num = 8000
-# image = images[num]
-# label = labels[num]
-# # 打印图片
-# print(mndata.display(image))
-# print('这张图片的实际数字是: ' + str(label))
+import cv2 
+images, labels = mndata.load_testing()
+num = 8000
+image = images[num]
+label = labels[num]
+# 打印图片
+print(mndata.display(image))
+print('这张图片的实际数字是: ' + str(label))
 
 def inverse_color(image):
 
@@ -58,10 +59,21 @@ def inverse_color(image):
 # 测试新图片，并输出预测值
 # mp = mpimg.imread('./1.png')
 
-_im = mnist.train.images[1,:]
-print(_im.shape)
-_im = _im.reshape(28,28)
-print(_im.shape)
+im = Image.open('./1.png').convert('L')
+
+# _im = mnist.train.images[1,:]
+# print(_im.shape)
+# _im = _im.reshape(28,28)
+
+# print(im.getdata())
+
+tv = list(im.getdata()) #get pixel values
+
+#normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
+tva = [ (255-x)*1.0/255.0 for x in tv] 
+# print([tva].shape)
+
+
 
 # img_gray = cv2.imread('./1.png')
 # img_gray = cv2.cvtColor(img_gray, cv2.COLOR_BGR2GRAY)
@@ -72,19 +84,19 @@ print(_im.shape)
 # plt.axis('off') # 不显示坐标轴
 # plt.show()
 
-plt.figure()
-plt.imshow(_im)
-plt.show()
+# plt.figure()
+# plt.imshow(_im)
+# plt.show()
 
 
 
 
 # print(mp.reshape(-1).shape)
-# print(np.array(image).reshape(1, 784))
+print(np.array(image).reshape(1, 784).shape)
 # print(img_gray)
 
-# y = tf.nn.softmax(y)  # 为了打印出预测值，我们这里增加一步通过softmax函数处理后来输出一个向量
-# result = sess.run(y, feed_dict={x: img_gray})  # result是一个向量，通过索引来判断图片数字
-# print('预测值为：')
-# print(result)
+y = tf.nn.softmax(y)  # 为了打印出预测值，我们这里增加一步通过softmax函数处理后来输出一个向量
+result = sess.run(y, feed_dict={x: [tva]})  # result是一个向量，通过索引来判断图片数字
+print('预测值为：')
+print(result)
 
---
+++ tensor11.py
@@ -0,0 +1,57 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)  # 插入数据
+import os 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+
+# # name在保存模型时非常有用
+# x = tf.placeholder("float", [None, 784], name='x') 
+# W = tf.Variable(tf.zeros([784, 10]), name='W')  
+# b = tf.Variable(tf.zeros([10]), name='b')
+# y = tf.nn.softmax(tf.matmul(x, W) + b, name='y')  # y预测概率分布
+
+# y_ = tf.placeholder("float", [None, 10])  # y_实际概率分布
+
+# cross_entropy = -tf.reduce_sum(y_ * tf.log(y))  # 交叉熵
+# # 梯度下降算法以0.01学习率最小化交叉熵
+# train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)  
+# init = tf.initialize_all_variables()  # 初始化变量
+
+# sess = tf.Session()
+# sess.run(init)
+# saver = tf.train.Saver()
+
+# for i in range(1000):  # 开始训练模型，循环1000次
+#     batch_xs, batch_ys = mnist.train.next_batch(100)
+#     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+# saver.save(sess, './tensor11/minst_model.ckpt')  # 保存模型
+
+#https://blog.csdn.net/u011389706/article/details/81223784
+
+from PIL import Image, ImageFilter
+import tensorflow as tf
+
+def imageprepare():
+    file_name = './2.png'  # 图片路径
+    myimage = Image.open(file_name).convert('L')  # 转换成灰度图
+    tv = list(myimage.getdata())  # 获取像素值
+    # 转换像素范围到[0 1], 0是纯白 1是纯黑
+    tva = [(255-x)*1.0/255.0 for x in tv] 
+    return tva
+
+result = imageprepare()
+init = tf.global_variables_initializer()
+saver = tf.train.Saver
+
+with tf.Session() as sess:
+    sess.run(init)
+    saver = tf.train.import_meta_graph('./tensor11/minst_model.ckpt.meta')  # 载入模型结构
+    saver.restore(sess,  './tensor11/minst_model.ckpt')  # 载入模型参数
+
+    graph = tf.get_default_graph()  # 计算图
+    x = graph.get_tensor_by_name("x:0")  # 从模型中获取张量x
+    y = graph.get_tensor_by_name("y:0")  # 从模型中获取张量y
+
+    prediction = tf.argmax(y, 1)
+    predint = prediction.eval(feed_dict={x: [result]}, session=sess)
+    print(predint[0])
